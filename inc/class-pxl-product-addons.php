66094bf77a5441158a72a339e5d1d727c7f7e422
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-05-24 10:20:52 +0700

    update

---
+++ inc/class-pxl-product-addons.php
@@ -0,0 +1,33 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Class_PXL_Product_Addons {
+
+	public function __construct() {
+		define( 'WC_PRODUCT_ADDONS_VERSION', '1.0.0' );
+		add_action( 'plugins_loaded', array( $this, 'init_classes' ) );
+		// add_action( 'init', array( $this, 'init_post_types' ), 20 );
+		// // Product Combos compatibility
+		// add_filter( 'woocommerce_combos_compatibility_modules', array( $this, 'init_pb_compatibility_module' ) );
+	}
+
+	public function init_classes() {
+		// Core (models)
+		// include_once( dirname( __FILE__ ) . '/includes/groups/class-lafka-product-addon-group-validator.php' );
+		// include_once( dirname( __FILE__ ) . '/includes/groups/class-lafka-product-addon-global-group.php' );
+		// include_once( dirname( __FILE__ ) . '/includes/groups/class-lafka-product-addon-product-group.php' );
+		// include_once( dirname( __FILE__ ) . '/includes/groups/class-lafka-product-addon-groups.php' );
+
+		// Admin
+		if ( is_admin() ) {
+			$this->init_admin();
+		}
+	}
+
+	protected function init_admin() {
+		include_once( dirname( __FILE__ ) . '/admin/pxl-product-addon-admin.php' );
+		$GLOBALS['PXL_Product_Addon_Admin'] = new PXL_Product_Addon_Admin();
+	}
+}
--- pxl-product-addons.php
+++ pxl-product-addons.php
@@ -7,6 +7,50 @@
  * Author: PXL Team
  * Author URI:  https://themeforest.net/user/7iquid/portfolio
  * Update URI:  https://7iquid.tech/
- * Text Domain: pxl-hotel-booking
+ * Text Domain: pxl-product-addons
  */
-?>
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+define('PHB_TEXT_DOMAIN', 'pxl-product-addons');
+define('PHB_VERSION', '1.0.1');
+define('PHB_NAME', 'PXL Hotel Booking');
+define('PHB_PATH', plugin_dir_path(__FILE__));
+define('PHB_URL', plugin_dir_url(__FILE__));
+define('PHB_PLUGIN_FILE', __FILE__ );
+
+class Pxl_Product_Addons {
+
+    public function __construct(){
+        $this->file = __FILE__;
+        $this->plugin_path = plugin_dir_path(__FILE__);
+        $this->plugin_url = plugin_dir_url(__FILE__);
+        $this->includes();
+        add_action( 'init', array( $this, 'init' ), 0 );  
+    }
+
+    public static function instance(){
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function includes() {
+        require_once( plugin_dir_path( __FILE__ ) . '/incl/addons/lafka-product-addons.php' );
+    }
+
+    public function init() {
+        $this->loaded_text_domain();
+        do_action( 'ppa_init' );
+    }
+}
+
+function pxl_pa(){
+    return Pxl_Product_Addons::instance();
+}
+
+// Install
+pxl_pa();
