6363ded6b9e6899aef17b6bbacbe722517de4169
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-05-24 08:45:18 +0700

    Create pxl-product-addons.php

---
+++ pxl-product-addons.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: PXL Product Addons
+ * Description: PXL Product Addons
+ * Plugin URI:  https://7iquid.tech/
+ * Version:     1.0.0
+ * Author:      PXL Team
+ * Author URI:  https://themeforest.net/user/7iquid/portfolio
+ * Update URI:  https://7iquid.tech/
+ * Text Domain: pxl-product-addons
+ */
+ 
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
