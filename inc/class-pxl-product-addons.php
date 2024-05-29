c50d5643b3562028334927b506046519c5b9471f
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-05-29 15:13:11 +0700

    Update class-pxl-product-addons.php

--- inc/class-pxl-product-addons.php
+++ inc/class-pxl-product-addons.php
@@ -26,10 +26,10 @@ class Class_PXL_Product_Addons {
 		}
 
 		// Front-side
-		include_once( dirname( __FILE__ ) . '/includes/class-lafka-product-addon-display.php' );
-		include_once( dirname( __FILE__ ) . '/includes/class-lafka-product-addon-cart.php' );
+		include_once( dirname( __FILE__ ) . '/admin//includes/class-lafka-product-addon-display.php' );
+		include_once( dirname( __FILE__ ) . '/admin//includes/class-lafka-product-addon-cart.php' );
 		// Helper class used by other plugins for compatibility
-		include_once( dirname( __FILE__ ) . '/includes/class-lafka-product-addons-helper.php' );
+		include_once( dirname( __FILE__ ) . '/admin//includes/class-lafka-product-addons-helper.php' );
 
 		$GLOBALS['Product_Addon_Display'] = new Lafka_Product_Addon_Display();
 		$GLOBALS['Product_Addon_Cart']    = new Lafka_Product_Addon_Cart();
